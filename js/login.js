44e6177361a56b72367008dba6c4a80efe8e3ab5
Gopi2301 <gopinaths2301@gmail.com>
2023-02-04 00:11:18 +0530

    guvi opp

--- js/login.js
+++ js/login.js
@@ -1,5 +1,25 @@
 var form = document.getElementById("myForm");
 
 function handleForm (event) {
-    
+    event.preventDefault();
+} 
+
+form.addEventListener('submit',handleForm)
+
+function insert (){
+    $ajax({
+        url: 'http://localhost/php%20demo/connect.php',
+        type:'POST',
+        data:{
+            name : $("input[name=name]").val(),
+            name : $("input[name=email]").val(),
+            name : $("input[name=password]").val(),
+            name : $("input[name=number]").val(),
+            action : "insert"
+        }, 
+        sucess: function(response){
+            alert("Data Added Successfully")
+        }
+    })
+    console.log("inserted")
 }
